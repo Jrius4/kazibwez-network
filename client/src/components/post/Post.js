2cfc46ff02b6c6d6f65ac4c089ed4932f1fe6474
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-09-29 18:44:28 +0300

    to completion

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,56 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux'
-import Spinner from '../common/Spinner';
-import {getPost} from '../../actions/postActions'
-import PostItem from '../posts/PostItem'
-import {Link} from 'react-router-dom'
+import PostItem from '../posts/PostItem';
 import CommentForm from './CommentForm';
 import CommentFeed from './CommentFeed';
+import Spinner from '../common/Spinner';
+import { getPost } from '../../actions/postActions';
 
 class Post extends Component {
-  
-    componentDidMount(){
-        this.props.getPost(this.props.match.params.id);
-    }
-  render() {
-      const {post,loading} =this.props.post;
-      let postContent;
+  componentDidMount() {
+    this.props.getPost(this.props.match.params.id);
+  }
 
-      if(post === null || loading || Object.keys(post).length === 0 ){
-          postContent = <Spinner/>
-      }else{
-        postContent =(
-          <div>
-            <PostItem post={post} showActions={false}/>
-            <CommentForm postId={post._id}/>
-            <CommentFeed postId={post._id} comments={post.comments}/>            
-          </div>
-        );
-      }
+  render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
+        </div>
+      );
+    }
 
     return (
       <div className="post">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-            <Link to="/feed" className="btn btn-sm btn-dark mb-3">
-             Back to Feed
-            </Link>
-            {postContent}
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back To Feed
+              </Link>
+              {postContent}
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
-Post.propTypes ={
-    getPost:PropTypes.func.isRequired,
-    post:PropTypes.object.isRequired
-}
-const mapStateToProps = state =>({
-    post:state.post
-})
 
-export default connect(mapStateToProps,{getPost})(Post);
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
